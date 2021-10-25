f4ff9afb91432cf5025e0d2d843b3d0ed324d1c3
dev-sweet <sweetali0520@gmail.com>
2021-10-26 01:49:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect to the mongodb
+const uri = "mongodb+srv://mydbuser1:0ophMlMQPNo4lDb9@cluster0.mas8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('nayikaCluster');
+        const usersCollection = database.collection('users');
+
+        // post API
+        app.post('/users',async(req,res)=>{
+            const newUser =  req.body;
+            const result = await usersCollection.insertOne(newUser);
+
+            console.log('Got an user from client side',newUser)
+            res.json(result);
+        });
+
+        // get API 
+        app.get('/users',async(req,res)=>{
+          const cursor = usersCollection.find({});
+          const users = await cursor.toArray();
+          res.send(users);
+        });
+        // get Every single user
+        app.get('/users/:id',async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)};
+          const user = await usersCollection.findOne((query));
+          
+          res.send(user);
+
+        });
+
+        // PUT method / Update 
+        app.put('/users/:id',async(req,res)=>{
+          const id = req.params.id;
+          const updatedUser = req.body;
+          const filter = {_id:ObjectId(id)};
+          const options = {upsert:true};
+          const updateDoc = {
+            $set:{
+              name:updatedUser.name,
+              email:updatedUser.email
+            }
+          }
+          const result = await usersCollection.updateOne(filter,updateDoc,options);
+        })
+        // Delete API 
+        app.delete('/users/:id',async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)};
+          const result = await usersCollection.deleteOne(query);
+          console.log('attempt to delete user',result)
+        })
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+app.get("/", (req, res) => {
+  res.send("Running my crud server");
+});
+
+app.listen(PORT, () => {
+  console.log("Crud is running at port ", PORT);
+});
+
+// 0ophMlMQPNo4lDb9            //mydbuser1
