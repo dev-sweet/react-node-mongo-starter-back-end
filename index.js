83b248ecf05ce55809b1a1d3db41129c9d23de88
dev-sweet <sweetali0520@gmail.com>
2021-10-28 00:47:26 +0600

    something went wrong

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
